e669ca6f7a487b1efda720971d77cf65f7868a9b
Jannick Joosten <JHA.Joosten@student.han.nl>
2020-08-07 17:01:29 +0200

    started working on the downloading of songs

--- src/bot.py
+++ src/bot.py
@@ -1,13 +1,16 @@
 import discord
 import html
 import json
+import patoolib
 from asyncio import TimeoutError
+from cgi import parse_header
 from discord.ext import commands
 from dotenv import load_dotenv
 from logging import getLogger, FileHandler, Formatter, DEBUG
-from os import getenv
+from os import getenv, path, makedirs, remove
+from pyunpack import Archive
 from urllib.parse import urlparse
-from urllib.request import Request, urlopen
+from urllib.request import Request, urlopen, urlretrieve
 
 # Setting up the logger
 logger = getLogger('discord')
@@ -25,8 +28,14 @@ bot = commands.Bot(command_prefix='$')
 
 # Globals
 DEFAULT_TIMEOUT = 15.0
+DOWNLOAD_PATH = './download'
+DOWNLOAD_PATH_TEMP = './temp'
 EMBED_COLOUR = discord.Colour.blue()
+REQUEST_HEADER = {'User-Agent': 'Mozilla/5.0'}
 apiUrl: str = None
+
+if not path.exists(DOWNLOAD_PATH_TEMP): makedirs(DOWNLOAD_PATH_TEMP)
+if not path.exists(DOWNLOAD_PATH): makedirs(DOWNLOAD_PATH)
 
 @bot.command(name='ping')
 async def pong(ctx):
@@ -57,7 +66,7 @@ async def search(ctx, search_string: str, query_type: str = None):
     # Search request
     apiRequest = Request(
         url='{0}search?query={1}%3D{2}'.format(apiUrl, query_type, search_string) if query_type else '{0}search?query={1}'.format(apiUrl, search_string),
-        headers={'User-Agent': 'Mozilla/5.0'}
+        headers=REQUEST_HEADER
     )
 
     with urlopen(apiRequest) as response:
@@ -87,6 +96,7 @@ async def search(ctx, search_string: str, query_type: str = None):
         songToDownload = apiData['songs'][int(msg.content) - 1]
         await ctx.send('Downloading: {0}'.format(songToDownload['name']))
         downloadSong(songToDownload)
+        await ctx.send('Downloaded: {0}'.format(songToDownload['name']))
     except TimeoutError:
         pass
 
@@ -96,6 +106,23 @@ async def on_command_error(ctx, error):
     await ctx.send_help(ctx.command)
 
 def downloadSong(songToDownload: dict):
-    pass
+    if songToDownload['directLinks'] and songToDownload['directLinks']['archive']:
+        request = Request(
+            url=songToDownload['directLinks']['archive'],
+            headers=REQUEST_HEADER
+        )
+
+        with urlopen(request) as file:
+            contentDisposition = file.info()['Content-Disposition']
+            t, p = parse_header(contentDisposition)
+            with open(DOWNLOAD_PATH_TEMP + '/' + p['filename'], 'wb') as filehandle:
+                filehandle.write(file.read());
+                songArchive = p['filename']
+
+        if songArchive:
+            songDirectory, fileExtension = path.splitext(songArchive)
+            if not path.exists(DOWNLOAD_PATH + '/' + songDirectory): makedirs(DOWNLOAD_PATH + '/' + songDirectory)
+            Archive(DOWNLOAD_PATH_TEMP + '/' + songArchive).extractall(DOWNLOAD_PATH + '/' + songDirectory)
+            remove(DOWNLOAD_PATH_TEMP + '/' + songArchive)
 
 bot.run(TOKEN)
