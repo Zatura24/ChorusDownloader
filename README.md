1d08cc72dea8860e5448eb61cf62b0f40d5f7019
Jannick Joosten <JHA.Joosten@student.han.nl>
2020-11-25 01:44:32 +0100

    Switched to config file and made style changes.
    Added a default configuration file that is always loaded, and therefor
    must be present.
    Applied python formatter and changed some code around.

--- README.md
+++ README.md
@@ -16,7 +16,18 @@ __Note__: requires 'admin' role
 
 `$search <search_string> [query_type]`
 Searches the api for a given 'search_string'. Optionaly a query_type can be supplied. These are currently the same as documented in Paturages API.  
-__Note__: If more than one word is needed for the search, the search_string needs to be encapsulated in quotes
+__Note__: If more than one word is needed for the search, the search_string needs to be encapsulated in quotes.
+
+Currently the following `query_type`s are available:
+- name
+- artist
+- album
+- genre
+- charter
+- tier_band
+- tier_guitarghl
+- diff_guitar
+- hasForced
 
 # Install
 Make sure to first create a discord bot, and have it's token ready. A tutorial can be found here: [Creating a bot account](https://discordpy.readthedocs.io/en/latest/discord.html). Also make sure to create an Admin role inside your discord server, because this necessary for setting the url.
@@ -35,12 +46,20 @@ docker run -d --name ChorusDownloader --env DISCORD_TOKEN=xxxx chorusdownloader
 
 ## Environment variables
 The following environment variables can be set during the docker run command:
-* Required
-    * DISCORD_TOKEN
-* Optional
-    * DISCORD_COMMAND_PREFIX
-    * CHORUS_API
-    * CHORUS_DOWNLOAD_PATH
-    * CHORUS_CACHE_EXPIRE_AFTER_SECONDS
+| variable      | required | default |
+| ------------- | -------- | ------- |
+| DISCORD_TOKEN | yes      |         |
 
 To add these to the docker run command use `--env KEY=VALUE`
+
+## Config file settings
+Located in the `config.ini` the following configurations are available:
+| variable                 | required | default                           |
+| ------------------------ | -------- | --------------------------------- |
+| apiUrl                   | false    | "https://chorus.fightthe.pw/api/" |
+| chunkSize                | true     | 8192                              |
+| defaultTimeout           | true     | 15.0                              |
+| discordCommandPrefix     | true     | $                                 |
+| downloadedSongsCacheFile | true     | ./downloaded_songs.txt            |
+| downloadPath             | true     | ./download                        |
+| downloadPathTemp         | true     | ./temp                            |
--- src/bot.py
+++ src/bot.py
@@ -1,167 +1,192 @@
 import discord
+import discord.ext.commands
 import patoolib
 import os
 import requests
-import requests_cache
 import logging
 from asyncio import TimeoutError
 from cgi import parse_header
-from discord.ext import commands
+from configparser import ConfigParser
 from dotenv import load_dotenv
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(filename='discord.log', level=logging.INFO)
 
-# Globals
-load_dotenv()
-API_URL: str = os.getenv('CHORUS_API') or None
-CHUNK_SIZE = 1024 * 8
-DEFAULT_TIMEOUT = 15.0
-DOWNLOADED_SONGS_FILE = './downloaded_songs.txt'
-DOWNLOAD_PATH = os.getenv('CHORUS_DOWNLOAD_PATH') or './download'
-DOWNLOAD_PATH_TEMP = './temp'
-EMBED_COLOUR = discord.Colour.blue()
-EXPIRE_AFTER_SECONDS = os.getenv('CHORUS_CACHE_EXPIRE_AFTER_SECONDS') or 60 * 60 * 24
+config = ConfigParser()
+config.read('config.ini')
+
+downloaded_songs_list = []
+
 REQUEST_HEADER = {'User-Agent': 'Mozilla/5.0'}
+API_URL = config['BOT']['apiUrl']
 
-# loading Discord Token from environment variables
-TOKEN = os.getenv('DISCORD_TOKEN')
+bot = discord.ext.commands.Bot(
+    command_prefix=config['BOT']['discordCommandPrefix'])
 
-# Creating bot
-bot = commands.Bot(command_prefix=os.getenv('DISCORD_COMMAND_PREFIX') or '$')
+if not os.path.exists(config['BOT']['downloadPathTemp']):
+    os.makedirs(config['BOT']['downloadPathTemp'])
+if not os.path.exists(config['BOT']['downloadPath']):
+    os.makedirs(config['BOT']['downloadPath'])
+if not os.path.isfile(config['BOT']['downloadedSongsCacheFile']):
+    os.mknod(config['BOT']['downloadedSongsCacheFile'])
+else:
+    with open(config['BOT']['downloadedSongsCacheFile'], 'r') as filehandle:
+        downloaded_songs_list = [int(line.rstrip('\n')) for line in filehandle]
 
-# Seting up request caching
-requests_cache.install_cache('chorus_cache', backend='sqlite', expire_after=EXPIRE_AFTER_SECONDS)
 
-# Setting up paths and files
-if not os.path.exists(DOWNLOAD_PATH_TEMP): os.makedirs(DOWNLOAD_PATH_TEMP)
-if not os.path.exists(DOWNLOAD_PATH): os.makedirs(DOWNLOAD_PATH)
-try:
-    filehandle = open(DOWNLOADED_SONGS_FILE, 'r')
-    DOWNLOADED_SONGS_LIST = [int(line.rstrip('\n')) for line in filehandle]
-except IOError:
-    with open(DOWNLOADED_SONGS_FILE, 'w'): DOWNLOADED_SONGS_LIST = []
+@bot.event
+async def on_command_error(ctx: discord.ext.commands.Context, error):
+    logging.exception(error)
+    await ctx.send("Oops! It looks like my human isn't a good programmer afterall... 🦄")
+
 
 @bot.command(name='ping')
 async def pong(ctx):
     await ctx.send('pong')
 
+
 @bot.command(name='api', help='Gets or sets the api')
-@commands.has_role('admin')
-async def api(ctx, api_url: str=None):
+@discord.ext.commands.has_role('admin')
+async def get_or_set_api(ctx: discord.ext.commands.Context, api_url: str = None):
     global API_URL
+
+    if not API_URL and not api_url:
+        return await ctx.send_help('api')
 
     if api_url:
-        if not api_url.endswith('/'): api_url += '/'
-        API_URL = api_url
-        await ctx.send('Api url set to: %s' % api_url)
-    elif API_URL:
-        await ctx.send('Api url is: %s' % API_URL)
+        API_URL = api_url + '/' if not api_url.endswith('/') else api_url
+        with open('config.ini', 'w') as configfile:
+            config['BOT']['apiUrl'] = API_URL
+            config.write(configfile)
+        await ctx.send('Api url set to: %s' % API_URL)
     else:
-        await ctx.send_help('api')
+        await ctx.send('Api url is: %s' % API_URL)
 
-@bot.command(name='search', help='Make a search using a string with an optional type. After that select a song by typing it\'s numerical value')
-async def search(ctx, search_string: str, query_type: str = None):
-    global API_URL
 
-    # Prevent calling before an api is added
-    if not API_URL: return await ctx.send('```Firstly add an api url with: $api <url>```')
+@bot.command(name='search', help='Make a search request using a string with an optional type. After that select a song by typing it\'s numerical value')
+async def search_and_download(ctx: discord.ext.commands.Context, search_string: str, query_type: str = None):
+    if not API_URL:
+        return await ctx.send('```First, add an api url with: {0}api <url>```'.format(config['DISCORD']['discordCommandPrefix']))
 
-    # Search request
-    apiData = getApiData(API_URL, search_string, query_type)
-    apiResultChoice = generateResultChoices(apiData)
+    # Make the search request and present the user with the response
+    apiData = get_api_data_json(API_URL, search_string, query_type)
+    apiResultChoice = generate_choise_result(apiData)
     apiResponse = discord.Embed(
         title=ctx.message.author.name,
-        colour=EMBED_COLOUR,
+        colour=discord.Colour.blue(),
         description='\n'.join(apiResultChoice)
     )
     apiResponse.set_footer(text='Type a number to download it')
-    await ctx.send(embed=apiResponse, delete_after=DEFAULT_TIMEOUT)
+    choisesMessage = await ctx.send(embed=apiResponse, delete_after=config['BOT'].getfloat('defaultTimeout'))
 
-    # Handle response
-    check = lambda message: message.content.isdigit() and 0 < int(message.content) <= len(apiData['songs']) and ctx.message.author.name == message.author.name
+    # Handeling the user response
+    def is_valid_respone(message):
+        return ctx.message.author.name == message.author.name and message.content.isdigit() and 0 < int(message.content) <= len(apiData['songs'])
 
     try:
-        msg = await bot.wait_for('message', check=check, timeout=DEFAULT_TIMEOUT)
-        songToDownload = apiData['songs'][int(msg.content) - 1]
+        responseMessage = await bot.wait_for('message', check=is_valid_respone, timeout=config['BOT'].getfloat('defaultTimeout'))
+        songToDownload = apiData['songs'][int(responseMessage.content) - 1]
 
-        async def sendDownloadMessage(message: str):
+        async def send_download_message(songToDownload: dict, message: str):
             return await ctx.send(message + '{0} - {1}'.format(songToDownload['name'], songToDownload['artist']))
 
-        if songToDownload['id'] in DOWNLOADED_SONGS_LIST:
-            await sendDownloadMessage('✅ Already downloaded: ')
+        if songToDownload['id'] in downloaded_songs_list:
+            return await send_download_message(songToDownload, '✅ Already downloaded: ')
         else:
-            downloadingMsg = await sendDownloadMessage('⌛ Downloading: ')
-            if downloadSong(songToDownload):
-                __addDownloadedSongToList(songToDownload['id'], DOWNLOADED_SONGS_LIST, DOWNLOADED_SONGS_FILE)
-                await sendDownloadMessage('✅ Downloaded: ')
+            await choisesMessage.delete()
+            downloadingMessage = await send_download_message(songToDownload, '⌛ Downloading: ')
+            if download_song(songToDownload):
+                await send_download_message(songToDownload, '✅ Downloaded: ')
             else:
-                await sendDownloadMessage('❌ Error downloading: ')
-            await downloadingMsg.delete()
+                await send_download_message(songToDownload, '❌ Error downloading: ')
+            await downloadingMessage.delete()
     except TimeoutError:
-        pass
+        pass  # ignoring because the user did not respond. Nothing went wrong
 
-@bot.event
-async def on_command_error(ctx, error):
-    logging.error(error)
-    await ctx.send("Oops! It looks like my human isn't a good programmer afterall... 🦄")
 
-def getApiData(apiUrl: str, search_string: str, query_type: str = None):
-    url=apiUrl + ('search?query={0}%3D"{1}"'.format(query_type, search_string) if query_type else 'search?query={0}'.format(search_string))
+def get_api_data_json(apiUrl: str, search_string: str, query_type: str = None):
+    url = apiUrl + 'search?query={0}%3D%22{1}%22'.format(
+        query_type, search_string) if query_type else apiUrl + 'search?query={0}'.format(search_string)
     return requests.get(url, headers=REQUEST_HEADER).json()
 
-def generateResultChoices(apiData: dict):
-    tierGuitar = lambda song : ' | '+'⭐'*int(song['tier_guitar']) if song['tier_guitar'] else ''
+
+def generate_choise_result(apiData: dict):
+    def tierGuitar(song): return ' | '+('⭐' * int(song['tier_guitar']))
     return [
-        '`{0}` {1} - {2}{3}'.format(i, song['name'], song['artist'], tierGuitar(song))
+        '`{0}` {1} - {2}{3}'.format(i, song['name'],
+                                    song['artist'], tierGuitar(song) if song['tier_guitar'] else '')
         for i, song in enumerate(apiData['songs'], start=1)
     ]
 
-def downloadSong(songToDownload: dict):
+
+def download_song(songToDownload: dict):
+    global downloaded_songs_list
+
     if 'directLinks' in songToDownload:
         if 'archive' in songToDownload['directLinks']:
-            __extractArchive(
-                __downloadSongToPath(songToDownload['directLinks']['archive'], DOWNLOAD_PATH_TEMP, CHUNK_SIZE), 
-                DOWNLOAD_PATH_TEMP, 
-                DOWNLOAD_PATH
+            extrach_archive(
+                download_song_to_path(
+                    songToDownload['directLinks']['archive'], config['BOT']['downloadPathTemp']),
+                config['BOT']['downloadPathTemp'],
+                config['BOT']['downloadPath']
             )
         else:
-            os.makedirs(os.path.join(DOWNLOAD_PATH, songToDownload['name']))
-            for key in songToDownload['directLinks']:
-                __downloadSongToPath(songToDownload['directLinks'][key], os.path.join(DOWNLOAD_PATH, songToDownload['name']), CHUNK_SIZE)
+            fullDownloadPath = os.path.join(
+                config['BOT']['downloadPath'], songToDownload['name'])
+            os.makedirs(fullDownloadPath)
+            for directLink in songToDownload['directLinks']:
+                download_song_to_path(
+                    songToDownload['directLinks'][directLink], fullDownloadPath)
+
+        downloaded_songs_list = cache_downloaded_song(songToDownload['id'],
+                                                      downloaded_songs_list,
+                                                      config['BOT']['downloadedSongsCacheFile'])
         return True
     return False
 
-def __downloadSongToPath(downloadLink: str, downloadPath: str, chunkSize: int):
+
+def download_song_to_path(downloadLink: str, downloadPath: str):
     with requests.get(downloadLink, headers=REQUEST_HEADER, stream=True) as response:
         # Bypass Google drive virus scanning warning
-        downloadWarning = next((key for key in response.cookies.get_dict() if key.startswith('download_warning')), None)
+        downloadWarning = check_for_download_warning(response)
         if downloadWarning:
-            response = requests.get(downloadLink+'&confirm='+response.cookies[downloadWarning], headers=REQUEST_HEADER, cookies=response.cookies, stream=True)
+            response = requests.get(downloadLink+'&confirm='+response.cookies[downloadWarning],
+                                    headers=REQUEST_HEADER,
+                                    cookies=response.cookies,
+                                    stream=True)
         response.raise_for_status()
 
-        filename = __getFilenameFromContentDisposition(response.headers.get('Content-Disposition'))
+        filename = get_filename_from_content_disposition(
+            response.headers.get('Content-Disposition'))
         downloadPath = os.path.join(downloadPath, filename)
 
-        # Save response to file
         with open(downloadPath, 'wb') as filehandle:
-            for chunk in response.iter_content(chunk_size=chunkSize):
+            for chunk in response.iter_content(chunk_size=config['BOT'].getint('chunkSize')):
                 filehandle.write(chunk)
-             
-        del response
-        return filename
 
-def __getFilenameFromContentDisposition(cd: str):
-    if not cd: return None
+    return filename
+
+
+def get_filename_from_content_disposition(cd: str):
     _, data = parse_header(cd)
     return data['filename'] if 'filename' in data else None
 
-def __extractArchive(filename: str, tempFolder: str, download_folder: str):
-    patoolib.extract_archive(os.path.join(tempFolder, filename), verbosity=-1, outdir=download_folder + '/', interactive=False)
-    os.remove(os.path.join(tempFolder, filename))
 
-def __addDownloadedSongToList(id: int, songList: list, songListFile: str):
-    songList.append(id)
+def extrach_archive(filename: str, tempFolder: str, download_folder: str):
+    tempFile = os.path.join(tempFolder, filename)
+    patoolib.extract_archive(tempFile, verbosity=-1,
+                             outdir=download_folder+'/', interactive=False)
+    os.remove(tempFile)
+
+
+def cache_downloaded_song(id: int, songList: list, songListFile: str):
     with open(songListFile, 'w') as filehandle:
         filehandle.writelines([str(songId) + "\n" for songId in songList])
 
-bot.run(TOKEN)
+    return songList + [id]
+
+
+def check_for_download_warning(response: requests.Response):
+    return next((link for link in response.cookies.get_dict() if link.startswith('download_warning')), None)
+
+
+bot.run(os.getenv('DISCORD_TOKEN'))
